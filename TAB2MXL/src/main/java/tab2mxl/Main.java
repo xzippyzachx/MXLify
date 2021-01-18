7db6cf539378dc641ff3eb894ff18a13d90a69ee
Zach <zacharyross1234@gmail.com>
2021-01-18 12:26:09 -0500

    Added GuitarTab.txt file Added simple file reader test

--- TAB2MXL/src/main/java/tab2mxl/Main.java
+++ TAB2MXL/src/main/java/tab2mxl/Main.java
@@ -1,11 +1,32 @@
 package tab2mxl;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Push test");
-		System.out.println("second push test");
-		System.out.println("third push test");
+		
+		ReadFile("GuitarTab.txt");	
+		
+	}
+	
+	public static void ReadFile(String fileName)
+	{
+		try {
+			File myObj = new File(fileName);
+			Scanner myReader = new Scanner(myObj);
+			while (myReader.hasNextLine()) {
+				String data = myReader.nextLine();
+				System.out.println(data);
+			}
+			myReader.close();
+		} 
+		catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
 	}
 
 }
