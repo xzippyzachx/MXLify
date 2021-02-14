e8a2893d20b1140dce1d11c4b72d1df4dd1b6191
Faruq Afolabi <gbolayo13@gmail.com>
2021-02-14 12:11:59 -0500

    Modified to support the tuning-octaves and the octaves

--- TAB2MXL/src/main/java/tab2mxl/FileGenerator.java
+++ TAB2MXL/src/main/java/tab2mxl/FileGenerator.java
@@ -158,7 +158,7 @@ public class FileGenerator {
 	 * @param clef
 	 * @param tune
 	 */
-	public void attributes(int division, int keySignature, int beat, int beatType, String clef, String[] tune) {
+	public void attributes(int division, int keySignature, int beat, int beatType, String clef, String[] tune, int[] tuneOctave) {
 		try {
 			myWriter.write(currentIndent + "<attributes>");
 			currentIndent += "  ";
@@ -215,9 +215,9 @@ public class FileGenerator {
 				myWriter.write(currentIndent + "<staff-tuning line=\"" + (i + 1) + "\">");
 				currentIndent += "  ";
 				newLine();
-				myWriter.write(currentIndent + "<tuning-step>" + tune[tune.length - i - 1] + "</tuning-step>");
+				myWriter.write(currentIndent + "<tuning-step>" + tune[tune.length - i - 1].toUpperCase() + "</tuning-step>");
 				newLine();
-				myWriter.write(currentIndent + "<tuning-octave>" + 2 + "</tuning-octave>");
+				myWriter.write(currentIndent + "<tuning-octave>" + tuneOctave[tuneOctave.length - i - 1] + "</tuning-octave>");
 				newLine();
 				tabBack();
 				myWriter.write(currentIndent + "</staff-tuning>");
@@ -245,7 +245,7 @@ public class FileGenerator {
 	 * @param fret
 	 * @param note
 	 */
-	public void addNote(int string, int fret, String note, String noteType, int duration)
+	public void addNote(int string, int fret, String note, String noteType, int duration, int octave)
 	{
 		try {
 			myWriter.write(currentIndent + "<note>");
@@ -256,7 +256,7 @@ public class FileGenerator {
 			newLine();
 			myWriter.write(currentIndent + "<step>" + note + "</step>");
 			newLine();
-			myWriter.write(currentIndent + "<octave>4</octave>");
+			myWriter.write(currentIndent + "<octave>" + octave + "</octave>");
 			newLine();
 			tabBack();
 			myWriter.write(currentIndent + "</pitch>"); 
@@ -282,7 +282,7 @@ public class FileGenerator {
 	 * @param chord
 	 * @param noteType
 	 */
-	public void addChord(char[] chord, String noteType, int duration) {
+	public void addChord(char[] chord, String noteType, int duration, int[] octaves) {
 		for (int i = 0; i<chord.length;i++) {
 			try {	
 				myWriter.write(currentIndent + "<note>");
@@ -297,7 +297,7 @@ public class FileGenerator {
 				newLine();
 				myWriter.write(currentIndent + "<step>" + chord[i] + "</step>");
 				newLine();
-				myWriter.write(currentIndent + "<octave>4</octave>");
+				myWriter.write(currentIndent + "<octave>" + octaves[i] + "</octave>");
 				newLine();
 				tabBack();
 				myWriter.write(currentIndent + "</pitch>"); 
--- TAB2MXL/src/main/java/tab2mxl/Parser.java
+++ TAB2MXL/src/main/java/tab2mxl/Parser.java
@@ -10,7 +10,11 @@ public class Parser {
 	int tabLineAmount;
 	private ArrayList<char[]> columns;
 	public static  Map<String,String> misc;
+	
+	//to be converted to user input
 	private static int beatType = 4;
+	private static int[] tuningOctave = {4,3,3,3,2,2};
+	//to be converted to user input
 	
 	//@SuppressWarnings("unused")
 	Parser(ArrayList<ArrayList<String>> input) {
@@ -61,6 +65,7 @@ public class Parser {
 		
 		
 		char[] chords = new char[stringAmount];
+		int[] chordsOctave = new int[stringAmount];
 		String type="";
 		fileGen.openPart(1);
 		int currentColumn = 0;
@@ -77,24 +82,28 @@ public class Parser {
 		char character = ' ';
 		int notesInColumn = 0;
 		char note = ' ';
+		int chordOctave = 0;
 		boolean chord;
 		String[] tune = new String[stringAmount];
+		int[] tO = new int[stringAmount];
+		
 		
 		/*adds the tuning of the strings to the tune array if the tuning is
 		 * specified in the TAB, or the default if it isn't*/
 		for(int i = 0; i < stringAmount; i++ ) {
 			//System.out.println(columns.get(0)[i]);
 			if(columns.get(0)[i] != '-' && columns.get(0)[i] != '|') {
-				tune[i] = Character.toString(columns.get(0)[i]).toUpperCase();				
+				tune[i] = Character.toString(columns.get(0)[i]);
+				tO[i] = Parser.tuningOctave[i];
 			}else {
 				tune = Tuning.getDefaultTuning(stringAmount);
+				tO = Tuning.getDefaultTuningOctave(stringAmount);
 				break;
 			}
 		}
 		
 		
-		Tuning tunner = new Tuning(tune, stringAmount);
-		
+		Tuning tunner = new Tuning(tune, stringAmount, tuningOctave);
 		
 		//Loop through the inputed columns
 		for(int i = 0; i < columns.size(); i++)
@@ -149,7 +158,7 @@ public class Parser {
 						fileGen.openMeasure(measure);
 						
 						if(measure ==1) {
-							fileGen.attributes(getDivisions(Integer.parseInt(misc.get("TimeSig"))), 0, Integer.parseInt(misc.get("TimeSig")), 4, "G", tune);
+							fileGen.attributes(getDivisions(Integer.parseInt(misc.get("TimeSig"))), 0, Integer.parseInt(misc.get("TimeSig")), 4, "G", tune, tuningOctave);
 						}
 					}
 				}			
@@ -167,11 +176,13 @@ public class Parser {
 					}
 					if (!chord) {
 						System.out.println("line " + line + " and fret " + fret);
-						fileGen.addNote(line, fret, tunner.getNote(tune[line-1].toUpperCase(), fret), noteType(beatNote), getDuration(beatNote));
+						fileGen.addNote(line, fret, tunner.getNote(tune[line-1], fret), noteType(beatNote), getDuration(beatNote), tunner.getOctave(tune[line-1], fret));
 					}
 					else {
-						note = tunner.getNote(tune[line-1].toUpperCase(), fret).charAt(0);
+						note = tunner.getNote(tune[line-1], fret).charAt(0);//doesn't the charAt(0) get rid of the sharps?
+						chordOctave = tunner.getOctave(tune[line-1], fret);
 						chords[j] = note;
+						chordsOctave[j] = chordOctave;
 						type = noteType(beatNote);
 						System.out.println("add chord " + line + " and fret " + fret);
 					}
@@ -183,7 +194,7 @@ public class Parser {
 			}
 			if (chord) {
 				double beatNote = (dash * beatTypeNote)/div;
-				fileGen.addChord(chords,type, getDuration(beatNote));
+				fileGen.addChord(chords,type, getDuration(beatNote), chordsOctave);
 			}
 			currentColumn++;
 		
@@ -205,6 +216,16 @@ public class Parser {
 		
 		for(Object t : cs) {
 			output = output && t.equals(o) ;
+		}
+		
+		return output;
+	}
+	
+	private boolean dot(double beatNote) {
+		boolean output = false;
+		
+		if(beatNote == 0.75) {
+			output = true;
 		}
 		
 		return output;
