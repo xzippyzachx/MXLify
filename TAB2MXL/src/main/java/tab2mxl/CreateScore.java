8883fbfebe06848f2fbb318aab8bbd2f8e8ad2fe
Yasser Al Zahed <yasseralzahed@gmail.com>
2021-02-17 20:02:22 -0500

    added opperating system check

--- TAB2MXL/src/main/java/tab2mxl/CreateScore.java
+++ TAB2MXL/src/main/java/tab2mxl/CreateScore.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 
 public class CreateScore {
 	String xmlPath;
-	
+	private static String OS = System.getProperty("os.name").toLowerCase();
+
 	CreateScore(String xmlPath){
+		if(isWindows()) {
 		this.xmlPath = xmlPath;
 		try {
 			Runtime.getRuntime().exec("ScoreMaker/dist/xml2score/xml2score.exe "+xmlPath);
@@ -14,5 +16,12 @@ public class CreateScore {
             System.out.println("didnt work");
         }
 	}
+		else {
+			System.out.println("unable to generate Score, this is a windows only feature");
+		}
+		}
+	public static boolean isWindows() {
+        return (OS.indexOf("win") >= 0);
+    }
 
 }
