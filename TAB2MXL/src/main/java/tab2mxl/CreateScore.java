a992dc4fb071bcce2c3bcdf1b0d90b4a1db21aee
Yasser Al Zahed <yasseralzahed@gmail.com>
2021-02-26 23:20:18 -0500

    changed save location of score to same folder as xml

--- TAB2MXL/src/main/java/tab2mxl/CreateScore.java
+++ TAB2MXL/src/main/java/tab2mxl/CreateScore.java
@@ -1,6 +1,8 @@
 package tab2mxl;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class CreateScore {
 	String xmlPath;
@@ -8,20 +10,24 @@ public class CreateScore {
 
 	CreateScore(String xmlPath){
 		if(isWindows()) {
-		this.xmlPath = xmlPath;
-		try {
-			Runtime.getRuntime().exec("ScoreMaker/dist/xml2score/xml2score.exe "+xmlPath);
-        } catch (IOException e) {
-            e.printStackTrace();  
-            System.out.println("didnt work");
-        }
-	}
+			this.xmlPath = xmlPath;
+			try {
+				Runtime.getRuntime().exec("ScoreMaker/dist/xml2score/xml2score.exe "+xmlPath);
+				TimeUnit.SECONDS.sleep(1); // Score.svg is generated in parallel so we need to wait for it to finish first
+				File score = new File("Score.svg");
+				score.renameTo(new File( new File(xmlPath).getParentFile() +"/"+ Parser.misc.get("Title")+"_Score.svg" ));
+				score.delete();
+			} catch (IOException | InterruptedException e) {
+				e.printStackTrace();
+				System.out.println("didnt work");
+			}
+		}
 		else {
 			System.out.println("unable to generate Score, this is a windows only feature");
 		}
-		}
+	}
 	public static boolean isWindows() {
-        return (OS.indexOf("win") >= 0);
-    }
+		return (OS.indexOf("win") >= 0);
+	}
 
 }
