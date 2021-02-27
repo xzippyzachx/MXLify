84298519a365fcb31ae265f3893678c384ee5a4a
Yasser Al Zahed <yasseralzahed@gmail.com>
2021-02-27 04:58:52 -0500

    removed deprecated function used more recient methods

--- TAB2MXL/src/main/java/tab2mxl/CreateScore.java
+++ TAB2MXL/src/main/java/tab2mxl/CreateScore.java
@@ -1,7 +1,9 @@
 package tab2mxl;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.concurrent.TimeUnit;
 
 public class CreateScore {
@@ -11,15 +13,28 @@ public class CreateScore {
 	CreateScore(String xmlPath){
 		if(isWindows()) {
 			this.xmlPath = xmlPath;
+			File script = new File("ScoreMaker/dist/xml2score/xml2score.exe");
+			String[] command = {script.getAbsolutePath(),xmlPath};
+			ProcessBuilder processbuilder = new ProcessBuilder(command);
+			File directory = new File("ScoreMaker");
+			processbuilder.directory(new File(directory.getAbsolutePath()));
 			try {
-				Runtime.getRuntime().exec("ScoreMaker/dist/xml2score/xml2score.exe "+xmlPath);
-				TimeUnit.SECONDS.sleep(1); // Score.svg is generated in parallel so we need to wait for it to finish first
-				File score = new File("Score.svg");
+				Process process = processbuilder.start();
+				BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+				String line;
+				while((line = reader.readLine()) != null){
+					System.out.println(line);
+				}
+				int exitCode = process.waitFor();
+				System.out.println("ExitCode = "+ exitCode );
+//				Runtime.getRuntime().exec("ScoreMaker/dist/xml2score/xml2score.exe "+xmlPath);
+//				TimeUnit.SECONDS.sleep(1); // Score.svg is generated in parallel so we need to wait for it to finish first
+				File score = new File("ScoreMaker/Score.svg");
 				score.renameTo(new File( new File(xmlPath).getParentFile() +"/"+ Parser.misc.get("Title")+"_Score.svg" ));
 				score.delete();
 			} catch (IOException | InterruptedException e) {
 				e.printStackTrace();
-				System.out.println("didnt work");
 			}
 		}
 		else {
