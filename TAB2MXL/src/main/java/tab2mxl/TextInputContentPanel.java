f8463e3ce4dc9475ad595fe1d9e638c986c6b68f
Yasser Al Zahed <yasseralzahed@gmail.com>
2021-02-27 21:23:43 -0500

    Added toggle for sheet music and warning on macOS

--- TAB2MXL/src/main/java/tab2mxl/TextInputContentPanel.java
+++ TAB2MXL/src/main/java/tab2mxl/TextInputContentPanel.java
@@ -151,7 +151,7 @@ public class TextInputContentPanel extends JPanel implements ActionListener {
 				}
 //				TextInputContentPanel.scoremake = e.getStateChange() == ItemEvent.SELECTED;
 				System.out.println(TextInputContentPanel.scoremake);
-				if( TextInputContentPanel.scoremake && CreateScore.isWindows()){
+				if( TextInputContentPanel.scoremake && !CreateScore.isWindows()){
 					JOptionPane.showMessageDialog(null,"We've detected you're running this application on MacOS, please read the User Manuel before using this feature to prevent any errors");
 //
 //
