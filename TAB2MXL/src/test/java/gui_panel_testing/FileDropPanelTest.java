c559fa87eda9b77f07cdb0d2431ef1689369c20b
Erika Grandy <erikafgrandy@hotmail.com>
2021-03-24 10:42:04 -0400

    Began GUI Testing, including automated testing using Swing Robot
    Some changes made to classes, changed modifiers to public for testing

---
+++ TAB2MXL/src/test/java/gui_panel_testing/FileDropPanelTest.java
@@ -0,0 +1,68 @@
+package gui_panel_testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.Color;
+
+import javax.swing.JLabel;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import gui_panels.FileDropPanel;
+
+class FileDropPanelTest {
+	
+	static FileDropPanel testPanel;
+	Color labelBackground = new Color(33,150,243);
+
+	@BeforeAll
+	static void setUp() throws Exception {
+		testPanel = new FileDropPanel();
+	}
+
+	@Test
+	void testElements() {
+		assertNotNull(FileDropPanel.getDropFilePath());
+		assertNotNull(testPanel.getDropPanel());
+		assertNotNull(testPanel.getDropLoc());
+		assertNotNull(testPanel.getDropLabel());
+	}
+	
+	@Test
+	void testDropLabel() {
+		assertTrue(testPanel.getDropLabel() instanceof JLabel);
+		
+		JLabel dropLabel = testPanel.getDropLabel();
+		assertEquals(dropLabel.getHorizontalAlignment(), JLabel.CENTER);
+		assertEquals(dropLabel.getHorizontalTextPosition(), JLabel.CENTER);
+		assertEquals(dropLabel.getAlignmentX(), JLabel.CENTER_ALIGNMENT);
+		
+		assertTrue(dropLabel.isOpaque());
+		assertTrue(dropLabel.isVisible());
+		
+		assertTrue(dropLabel.getText().endsWith("Drop Tablature Text File"));
+		assertEquals(dropLabel.getBackground(), labelBackground);
+	}
+	
+	@Test
+	void testDropLoc() {
+		assertTrue(testPanel.getDropLoc() instanceof JLabel);
+		
+		JLabel dropLoc = testPanel.getDropLoc();
+		
+	//	assertEquals(dropLoc.getWidth(), 120);
+	//	assertEquals(dropLoc.getHeight(), 300);
+		
+		assertNotNull(dropLoc.getIcon());
+		assertEquals(dropLoc.getIcon().getIconHeight(),120);
+		assertEquals(dropLoc.getIcon().getIconWidth(),140);
+		
+		assertEquals(dropLoc.getBackground(), Color.LIGHT_GRAY);
+		
+	}
+	
+	
+
+}
